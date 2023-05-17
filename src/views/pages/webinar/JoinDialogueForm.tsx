68c69d01667214abda00320e011806642e09e94a
ismail-nabaji <91204944+ismail-nabaji@users.noreply.github.com>
2023-05-17 13:08:58 +0200

    added close icon

--- src/views/pages/webinar/JoinDialogueForm.tsx
+++ src/views/pages/webinar/JoinDialogueForm.tsx
@@ -1,6 +1,8 @@
 import Dialog from '@mui/material/Dialog'
 import DialogContent from '@mui/material/DialogContent'
 import DialogTitle from '@mui/material/DialogTitle'
+import IconButton from '@mui/material/IconButton'
+import Icon from 'src/@core/components/icon'
 
 interface WebinarFormProps {
   open: boolean;
@@ -12,6 +14,15 @@ const WebinarForm: React.FC<WebinarFormProps> = ({ open, onClose }) => {
       <Dialog open={open} onClose={() => onClose(false)}>
         <DialogTitle>Webinar Confirmation</DialogTitle>
         <DialogContent>
+          <IconButton
+              size='small'
+              onClick={() => {
+                onClose(false)
+              }}
+              sx={{ position: 'absolute', right: '1rem', top: '1rem' }}
+            >
+            <Icon icon='mdi:close' />
+          </IconButton>
           <p>Are you sure you want to join the webinar?</p>
         </DialogContent>
       </Dialog>
