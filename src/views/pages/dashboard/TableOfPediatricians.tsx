b8501021ca79150c1db5ff00891faf6d761a0ecb
TiiMA1 <fatimaouaatou2000@gmail.com>
2023-07-20 15:50:54 +0100

    degrade comments in PediatricianTable

--- src/views/pages/dashboard/TableOfPediatricians.tsx
+++ src/views/pages/dashboard/TableOfPediatricians.tsx
@@ -7,6 +7,12 @@ interface TableOfPediatriciansProps {
   cityValue: string
 }
 
+interface Sentiments {
+  label: string;
+  score: number;
+}
+
+
 export interface TableBodyRowType {
   _id: number
   name: string
@@ -16,6 +22,7 @@ export interface TableBodyRowType {
   reviews_average: number
   phone_number: number
   city: string
+  sentiments:Sentiments[]
 }
 
 interface CellType {
@@ -64,9 +71,26 @@ const TableOfPediatricians = (props: TableOfPediatriciansProps): ReactElement =>
     }
   }
 
- 
 
  
+  const getColorFromScore = (score: number,label:string) => {
+    // Fonction pour déterminer la couleur en fonction du score
+    // Rouge pour les sentiments négatifs, Vert pour les sentiments positifs
+    const redNeg = Math.round(score* 255);//Rapproche du rouget lorsque le score est élevé
+    const greenNeg = Math.round((1 - score)  * 255);// Rapproche du vert lorsque le score est bas
+    const redPos = Math.round((1 - score) * 255); // Rapproche du rouge lorsque le score est bas
+    const greenPos = Math.round(score * 255); // Rapproche du vert lorsque le score est élevé
+    if (label=='POSITIVE'){
+      return `rgb(${redPos}, ${greenPos}, 0)`
+    }
+    if (label=='NEGATIVE'){
+      return `rgb(${redNeg}, ${greenNeg}, 0)`
+  
+    }
+    ;
+  };
+
+  
 
   const columns: GridColDef[] = [
     {
@@ -75,6 +99,7 @@ const TableOfPediatricians = (props: TableOfPediatriciansProps): ReactElement =>
       minWidth: 300,
       headerName: 'Name',
       renderCell: ({ row }: CellType) => {
+        
         return (
           <div style={{ display: 'flex', alignItems: 'center' }}>
             {renderUserAvatar(row)}
@@ -85,6 +110,7 @@ const TableOfPediatricians = (props: TableOfPediatriciansProps): ReactElement =>
         )
       }
     },
+    
     {
       flex: 0.2,
       minWidth: 300,
@@ -102,8 +128,19 @@ const TableOfPediatricians = (props: TableOfPediatriciansProps): ReactElement =>
       renderCell: ({ row }: CellType) => (
         <span style={{ fontSize: '0.875rem' }}>{row.phone_number}</span>
       )
-    }
-  ]
+    },
+    {
+      flex: 0.2,
+      minWidth: 300,
+      field: 'Avis',
+      headerName: 'Avis',
+      renderCell: ({ row }: CellType) => (
+        <span style={{ fontSize: '0.875rem', color: getColorFromScore(row.sentiments[0].score,row.sentiments[0].label) }}>
+          {row.sentiments[0].label}
+        </span>
+      ),
+    },
+  ];
   const getRowId = (row: TableBodyRowType) => row._id;
 
   return (
@@ -115,6 +152,7 @@ const TableOfPediatricians = (props: TableOfPediatriciansProps): ReactElement =>
         disableSelectionOnClick
         getRowId={getRowId}
       />
+        
     </div>
   )
 }
