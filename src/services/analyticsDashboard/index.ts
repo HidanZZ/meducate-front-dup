1dcea0fb40b6f39af65554d3778b7fd8da939069
TiiMA1 <fatimaouaatou2000@gmail.com>
2023-07-30 02:21:29 +0100

    ApexColumnChart

--- src/services/analyticsDashboard/index.ts
+++ src/services/analyticsDashboard/index.ts
@@ -88,5 +88,17 @@ class AnalyticsDashboard {
       }
     }
 
+
+    static async  getMedicalDataCountsByCity(){
+      try {
+        const response = await fetch('http://localhost:8000/getMedicalDataCountsByCity');
+        const data = await response.json();
+        return data;
+      } catch (error) {
+        console.error('Erreur lors de la récupération des données:', error);
+        throw new Error('Erreur serveur');
+      }
+    }
+
   }
   export default AnalyticsDashboard
